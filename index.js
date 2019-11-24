4618ef48d2e82c9901daf65589fe1f0fb9a39148
audiatan <58123327+audiatan@users.noreply.github.com>
2019-11-23 23:19:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ client.on('message', msg => {
     msg.reply('https://youtu.be/aALk6BJw8J4')
   }else if (msg.content === 'bob give us a fish fact') {
     msg.reply('one day fish will take over the world')
-  }else if (msg.content === 'bob i love you') {
+  }else if (msg.content === 'bob I love you') {
     msg.reply('duck you')
   }else if (msg.content === 'bob help') {
     msg.reply('you get none')
   }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.token);
