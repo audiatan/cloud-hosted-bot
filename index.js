aba4606bd77a8a46ada7cc9d7ee694406d07bd44
audiatan <58123327+audiatan@users.noreply.github.com>
2019-11-23 23:17:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === 'help') {
-    msg.reply('you get none');
+    msg.reply('https://docs.google.com/document/d/1fcc7j54zpbVQ9wVXdcTA7EL-03jbVltU-qP4l5FC7JQ/edit');
   } else if (msg.content === 'bob pay respects') {
     msg.reply('f');
-  } else if (msg.content === 'say hi') {
+  } else if (msg.content === 'bob say hi') {
     msg.reply('no')
   }else if (msg.content === 'del taco serves') {
     msg.reply('FR E SH A VOCA DO')
   }else if (msg.content === 'how tall is audria') {
     msg.reply('taller than you')
-  }else if (msg.content === 'whats 17-8') {
+  }else if (msg.content === 'bob whats 17-8') {
     msg.reply('5')
-  }else if (msg.content === 'who made you') {
+  }else if (msg.content === 'bob who made you') {
     msg.reply('ur mom')
   }else if (msg.content === 'this is so sad bob play despacito') {
     msg.reply('https://youtu.be/kJQP7kiw5Fk')
-  }else if (msg.content === 'whats your favorite song bob') {
+  }else if (msg.content === 'bob whats your favorite song') {
     msg.reply('https://youtu.be/aALk6BJw8J4')
   }else if (msg.content === 'bob give us a fish fact') {
     msg.reply('one day fish will take over the world')
   }else if (msg.content === 'bob i love you') {
     msg.reply('duck you')
-  }else if (msg.content === 'bob') {
-    msg.reply('https://docs.google.com/document/d/1fcc7j54zpbVQ9wVXdcTA7EL-03jbVltU-qP4l5FC7JQ/edit')
+  }else if (msg.content === 'bob help') {
+    msg.reply('you get none')
   }
 });
 
